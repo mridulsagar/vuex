f0ce161d21a19d97ad35eaff4b282b09f0faf5a2
mridul <mridulsagar699@gmail.com>
2019-02-26 01:53:45 +0600

    Lesson 1: A Simple Vue App

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two v-bind:products="products"></product-list-two>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductListOne from "./components/ProductListOne.vue";
+import ProductListTwo from "./components/ProductListTwo.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    "product-list-one": ProductListOne,
+    "product-list-two": ProductListTwo
+  },
+  data() {
+    return {
+      products: [
+        { name: "Banana", price: 20 },
+        { name: "Orange", price: 40 },
+        { name: "Apple", price: 60 },
+        { name: "Mango", price: 80 }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  color: #555;
 }
 </style>
